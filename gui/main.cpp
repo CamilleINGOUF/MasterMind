1182fce8bc04c48c4cdb4cb218cc94667e6dbd96
AxelSegard <segard.isn@gmail.com>
2017-06-07 10:04:27 +0200

    fond d ecran

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,24 +1,32 @@
-#include <SFML/Graphics.hpp>
+#include "SFML/Graphics.hpp"
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+int main(int argc, char** argv){
+  sf::RenderWindow renderWindow(sf::VideoMode(972, 640), "Mastermind");
+  sf::Texture texture;
+  if (!texture.loadFromFile("../img/mastermind.jpg")){
+    renderWindow.close();
+  }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+  sf::Sprite background(texture);
 
-    return 0;
+  while (renderWindow.isOpen()){
+    sf::Event event;
+
+    while (renderWindow.pollEvent(event)){
+      if (event.type == sf::Event::EventType::Closed)
+      {
+        renderWindow.close();
+      }
+
+      if (event.type == sf::Event::EventType::KeyPressed)
+      {
+      }
+    } 
+
+      renderWindow.clear(sf::Color::White);
+      renderWindow.draw(background);
+      renderWindow.display();
+
+  }
 }
