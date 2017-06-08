c996cc11c379c35b8180851dcbd966df0ef4a0cc
Ingouf <ingouf.camille@gmail.com>
2017-06-08 09:23:40 +0200

    class Button

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,5 +1,7 @@
 #include "SFML/Graphics.hpp"
 
+#include <iostream>
+#include "buttonMM.hpp"
 
 int main(int argc, char** argv){
   sf::RenderWindow renderWindow(sf::VideoMode(972, 640), "Mastermind");
@@ -10,22 +12,39 @@ int main(int argc, char** argv){
 
   sf::Sprite background(texture);
 
+  bool pressed = false;
+
+  //sf::RectangleShape rectangle(sf::Vector2f(120, 50));
+  ButtonMM button(100,100,180,150,"Oui");
+
   while (renderWindow.isOpen()){
     sf::Event event;
-
     while (renderWindow.pollEvent(event)){
       if (event.type == sf::Event::EventType::Closed)
-      {
-        renderWindow.close();
-      }
+	{
+	  renderWindow.close();
+	}
+      if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	{
+	  if(!pressed)
+	    {
+	     
+	      pressed = true;
 
-      if (event.type == sf::Event::EventType::KeyPressed)
-      {
-      }
+	      if(button.isClicked(sf::Mouse::getPosition(renderWindow).x,
+				  sf::Mouse::getPosition(renderWindow).y))
+		std::cout << "Button clicked" << std::endl;
+	    }
+	}
+      else
+	{
+	  pressed = false;
+	}
     } 
 
       renderWindow.clear(sf::Color::White);
       renderWindow.draw(background);
+      renderWindow.draw(button);
       renderWindow.display();
 
   }
