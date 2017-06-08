0cfce083ebe992fa58d641e89a210073f09da729
AxelSegard <segard.isn@gmail.com>
2017-06-08 09:34:54 +0200

    Menu interactif

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,31 +1,66 @@
 #include "SFML/Graphics.hpp"
+#include <iostream>
+#include "menu.h"
 
 
-int main(int argc, char** argv){
+int main(){
   sf::RenderWindow renderWindow(sf::VideoMode(972, 640), "Mastermind");
-  sf::Texture texture;
+
+  Menu menu(renderWindow.getSize().x, renderWindow.getSize().y);
+  
+  /*sf::Texture texture;
   if (!texture.loadFromFile("../img/mastermind.jpg")){
     renderWindow.close();
   }
 
   sf::Sprite background(texture);
 
+  sf::RectangleShape rectangle(sf::Vector2f(120, 50));
+  */
   while (renderWindow.isOpen()){
     sf::Event event;
 
     while (renderWindow.pollEvent(event)){
-      if (event.type == sf::Event::EventType::Closed)
-      {
-        renderWindow.close();
-      }
+      switch (event.type)
+	{
+	case sf::Event::KeyReleased:
+	  switch (event.key.code)
+	    {
+	    case sf::Keyboard::Up:
+	      menu.MoveUp();
+	      break;
 
-      if (event.type == sf::Event::EventType::KeyPressed)
-      {
-      }
-    } 
+	    case sf::Keyboard::Down:
+	      menu.MoveDown();
+	      break;
 
-      renderWindow.clear(sf::Color::White);
-      renderWindow.draw(background);
+	    case sf::Keyboard::Return:
+	      switch (menu.GetPressedItem())
+		{
+		case 0:
+		  std::cout<< " Play appuyé" << std::endl;
+		  break;
+		case 1:
+		  std::cout<< " Options appuyé" << std::endl;
+		  break;
+		case 2:
+		  renderWindow.close();
+		  break;
+		}
+	    }
+	  break;
+	case sf::Event::Closed:
+	  renderWindow.close();
+	  break;
+
+	}
+    }
+
+
+      renderWindow.clear();
+      /*renderWindow.draw(background);
+	renderWindow.draw(rectangle);*/
+      menu.draw(renderWindow);
       renderWindow.display();
 
   }
